a5db0eebb221e32c6acaa60e363febc8725f8d41
Marjolein <marjolein.t.oostrom@gmail.com>
2025-12-17 22:26:36 -0800

    change title

--- frontend/src/components/WorldMap.js
+++ frontend/src/components/WorldMap.js
@@ -289,7 +289,7 @@ const WorldMap = ({
 
         <div className="control-overlay" style={{ '--card-bg': COLORS.cardBg, '--text-color': COLORS.text, '--glow-color': COLORS.glow, '--border-color': COLORS.border }}>
           <div className="overlay-title">
-            {mode==='quiz' && targetCountryName ? `Find: ${targetCountryName}` : 'Click Countries'}
+            {mode==='quiz' && targetCountryName ? `Find: ${targetCountryName}` : 'Click countries to see info or turn on quiz mode'}
           </div>
 
           <div className="overlay-controls">
